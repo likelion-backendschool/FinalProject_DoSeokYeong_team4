d562fb465340a4cec51b598b02bfe9cd95bf6cc7
DOSEOKYEONG <dho88dho@naver.com>
2022-10-31 14:50:40 +0900

    FEAT#33 : 정산품목 데이터의 날짜 넘어오도록

--- finalproject_3week/src/main/java/com/ll/exam/finalproject/app/home/controller/AdmHomeController.java
+++ finalproject_3week/src/main/java/com/ll/exam/finalproject/app/home/controller/AdmHomeController.java
@@ -2,8 +2,7 @@ package com.ll.exam.finalproject.app.home.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/adm")
@@ -18,6 +17,21 @@ public class AdmHomeController {
     @PreAuthorize("hasAuthority('ADMIN')")
     public String showMain() {
         return "adm/home/main";
+    }
+
+    @GetMapping("/rebate/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public String showMakeData() {
+        return "adm/home/makeData";
+    }
+
+    @PostMapping("/rebate/makeData")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @ResponseBody
+    public String createMakeData(@RequestParam String yearMonth) {
+        // yearMonth로 년, 월값이 넘어옴 ex) 2022-03
+
+        return yearMonth;
     }
 }
 
